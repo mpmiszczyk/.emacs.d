6b04f122ea04d19800571e7b187300fee5b7088a
mpm <mpmiszczyk@gmail.com>
2013-12-22 16:23:12 +0100

    Moved mail configuration to another file.
    Not really using it too much, but it seems to be good exercise.

--- init.el
+++ init.el
@@ -54,66 +54,8 @@
 (setq tramp-syntax 'url)
 (setq tramp-default-method "ssh")
 
-(require 'mu4e)
-
-;; default
-(setq mu4e-maildir "~/Maildir")
-
-(setq mu4e-drafts-folder "/[Gmail].Wersje_robocze")
-(setq mu4e-sent-folder   "/[Gmail].Wyslane")
-(setq mu4e-trash-folder  "/[Gmail].Trash")
-
-;; don't save message to Sent Messages, Gmail/IMAP takes care of this
-(setq mu4e-sent-messages-behavior 'delete)
-
-;; setup some handy shortcuts
-;; you can quickly switch to your Inbox -- press ``ji''
-;; then, when you want archive some messages, move them to
-;; the 'All Mail' folder by pressing ``ma''.
-
-(setq mu4e-maildir-shortcuts
-      '( ("/INBOX"               . ?i)
-         ("/[Gmail].Wersje_robocze"   . ?s)
-         ("/[Gmail].Trash"       . ?t)
-         ("/[Gmail].Wszystkie"    . ?a)))
-
-;; allow for updating mail using 'U' in the main view:
-(setq mu4e-get-mail-command "offlineimap"
-      mu4e-update-interval 300)   ;;upfate every 5 minutes
-
-;; something about ourselves
-(setq
- user-mail-address "mpmiszczyk@gmail.com"
- user-full-name  "marcin piotr miszczyk"
- message-signature "")
-
-;; sending mail -- replace USERNAME with your gmail username
-;; also, make sure the gnutls command line utils are installed
-;; package 'gnutls-bin' in Debian/Ubuntu
-
-(require 'smtpmail)
-;; (setq message-send-mail-function 'smtpmail-send-it
-;;       starttls-use-gnutls t
-;;       smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
-;;       smtpmail-auth-credentials
-;;       '(("smtp.gmail.com" 587 "USERNAME@gmail.com" nil))
-;;       smtpmail-default-smtp-server "smtp.gmail.com"
-;;       smtpmail-smtp-server "smtp.gmail.com"
-;;       smtpmail-smtp-service 587)
-
-;; alternatively, for emacs-24 you can use:
-(setq message-send-mail-function 'smtpmail-send-it
-    smtpmail-stream-type 'starttls
-    smtpmail-default-smtp-server "smtp.gmail.com"
-    smtpmail-smtp-server "smtp.gmail.com"
-    smtpmail-smtp-service 587)
-;; don't save messages to Sent Messages, Gmail/IMAP takes care of this
-(setq mu4e-sent-messages-behavior 'delete)
-
-;; don't keep message buffers around
-(setq message-kill-buffer-on-exit t)
-
-
+(add-to-list 'load-path "~/.emacs.d")
+(require 'conf-mu4e)
 
 (require 'tramp)
 
