03db3aa99d3acfe43f7cd3fa817c21738f34878e
mpm <mpmiszczyk@gmail.com>
2013-12-22 16:04:37 +0100

    Set ticking sound during tasks.
    Seems nice.  Could help keep me concentrated.

--- init.el
+++ init.el
@@ -30,6 +30,8 @@
 (global-rainbow-delimiters-mode)
 (ido-vertical-mode)
 
+(setq org-pomodoro-play-ticking-sounds t)
+
 ;; scroll one line at a time (less "jumpy" than defaults)
 
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
