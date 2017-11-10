64e1ded7888d9c98dc2640758e4c51a379d95fd7
Marcin Miszczyk <mpmiszczyk@gmail.com>
2017-11-10 14:29:04 +0100

    Start with JavaScript workflow.

--- lisp/conf-js.el
+++ lisp/conf-js.el
@@ -19,3 +19,15 @@
 
 (add-hook 'js2-mode-hook (lambda ()
   (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
+
+(require 'company)
+(require 'company-tern)
+
+(add-to-list 'company-backends 'company-tern)
+(add-hook 'js2-mode-hook (lambda ()
+                           (tern-mode)
+                           (company-mode)))
+
+;; Disable completion keybindings, as we use xref-js2 instead
+(define-key tern-mode-keymap (kbd "M-.") nil)
+(define-key tern-mode-keymap (kbd "M-,") nil)
