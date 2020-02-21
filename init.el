07fca28566943249f56078e37cc7e7a718db3bab
marcin miszczyk <mpmiszczyk@gmail.com>
2020-02-21 08:37:07 +0100

    Stop making sounds on cancel and error.

--- init.el
+++ init.el
@@ -17,11 +17,14 @@
 (setq custom-file (make-temp-file "emacs-custom"))
 (load custom-file)
 
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
+(use-package emacs
+  :config
+  (tool-bar-mode -1)
+  (menu-bar-mode -1)
+  (scroll-bar-mode -1)
+  (setq inhibit-startup-message t)
+  (setq visible-bell t))
 
-(setq inhibit-startup-message t)
 
 (use-package nord-theme)
 
