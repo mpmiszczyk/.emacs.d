e2d9b4d2ce33652c27ff760a5a8f8b4b4388d3b3
marcin piotr miszczyk <mpmiszczyk@gmail.com>
2020-09-22 22:02:47 +0200

    Start using spell check

--- init.el
+++ init.el
@@ -149,6 +149,11 @@
   :bind
   ("C-c d" . ispell-change-dictionary))
 
+(use-package flyspell
+  :config
+  (add-hook 'text-mode-hook #'flyspell-mode)
+  (add-hook 'prog-mode-hook #'flyspell-prog-mode))
+
 (use-package dockerfile-mode)
 (use-package docker-compose-mode)
 
