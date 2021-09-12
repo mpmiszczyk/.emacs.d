1a5987e3b79e65a08fa119029fe5ceb2afd47cf5
marcin piotr miszczyk <mpmiszczyk@gmail.com>
2021-09-13 01:27:27 +0200

    Lets roam; or at least journal

--- init.el
+++ init.el
@@ -266,6 +266,26 @@
 
 (require 'org-mode-config)
 
+(use-package org-roam
+  :init
+  (setq org-roam-v2-ack t)
+  :custom
+  (org-roam-directory "~/roam")
+  (org-roam-completion-everywhere t)
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n i" . org-roam-node-insert)
+         :map org-mode-map
+         ("C-M-i" . completion-at-point)
+         :map org-roam-dailies-map
+         ("Y" . org-roam-dailies-capture-yesterday)
+         ("T" . org-roam-dailies-capture-tomorrow))
+  :bind-keymap
+  ("C-c n d" . org-roam-dailies-map)
+  :config
+  (require 'org-roam-dailies) ;; Ensure the keymap is available
+  (org-roam-db-autosync-mode))
+
 (use-package elfeed)
 (use-package s)
 
