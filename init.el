8f7bb58a75b636a39993bd8586d07dcc5c727169
mpmiszczyk <1692426+mpmiszczyk@users.noreply.github.com>
2025-01-25 23:09:58 +0100

    Corfu for completitions

--- init.el
+++ init.el
@@ -540,6 +540,37 @@ is loaded dynamiclly"
           #'ivy-format-function-line)
   :hook (after-init . ivy-rich-mode))
 
+(use-package corfu
+  ;; Optional customizations
+  ;; :custom
+  ;; (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
+  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
+  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
+  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
+  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+
+  ;; Enable Corfu only for certain modes. See also `global-corfu-modes'.
+  ;; :hook ((prog-mode . corfu-mode)
+  ;;        (shell-mode . corfu-mode)
+  ;;        (eshell-mode . corfu-mode))
+
+  ;; Recommended: Enable Corfu globally.  This is recommended since Dabbrev can
+  ;; be used globally (M-/).  See also the customization variable
+  ;; `global-corfu-modes' to exclude certain modes.
+  :init
+  (global-corfu-mode))
+
+(use-package corfu-prescient
+  :after (corfu prescient)
+  :config
+  (corfu-prescient-mode))
+
+(use-package nerd-icons-corfu
+  :after corfu
+  :config
+  (add-hook 'corfu-update-hook 'nerd-icons-corfu--update))
+
 (use-package prescient
   :config
   (setq prescient-history-length 2000)
